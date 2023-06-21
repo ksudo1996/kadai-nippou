ed3a29c075cbd4fedfa2760946f7c70d82b2ef28
kohei.sudo <ksudo@unisrv.jp>
2023-06-21 17:27:37 +0900

    Employeeモデル、EmployeeViewモデル作成

--- src/main/java/constants/JpaConst.java
+++ src/main/java/constants/JpaConst.java
@@ -14,6 +14,7 @@ public interface JpaConst {
 
     // 従業員テーブル
     String TABLE_EMP = "employess"; // テーブル名
+    // 従業員テーブルカラム
     String EMP_COL_ID = "id"; // id
     String EMP_COL_CODE = "code"; // 社員番号
     String EMP_COL_NAME = "name"; // 氏名
@@ -24,7 +25,7 @@ public interface JpaConst {
     String EMP_COL_DELETE_FLAG = "delete_flag";// 削除フラグ
 
     int ROLE_ADMIN = 1; // 管理者権限ON(管理者)
-    int ROLE_GENEAL = 0; // 管理者権限OFF(一般)
+    int ROLE_GENERAL = 0; // 管理者権限OFF(一般)
     int EMP_DEL_TRUE = 1; // 削除フラグON(削除済み)
     int EMP_DEL_FALSE = 0; // 削除フラグOFF(現役)
 
@@ -51,16 +52,16 @@ public interface JpaConst {
     // NamedQuery の nameとquery
     // 全ての従業員をidの降順に取得する
     String Q_EMP_GET_ALL = ENTITY_EMP + ".getAll"; //name
-    String Q_EMP_GET_ALL_DEF = "SELECT e FROM Enpyee AS e ORDER BY e.id DESC"; //query
+    String Q_EMP_GET_ALL_DEF = "SELECT e FROM Employee AS e ORDER BY e.id DESC"; //query
     // 全ての従業員を取得する
     String Q_EMP_COUNT = ENTITY_EMP + ".count";
     String Q_EMP_COUNT_DEF = "SELECT COUNT(e) FROM Employee AS e";
     // 社員番号とハッシュ化済パスワードを条件に未削除の従業員を取得する
     String Q_EMP_GET_BY_CODE_AND_PASS = ENTITY_EMP + ".getByCodeAndPass";
-    String Q_EMP_GET_BY_CODE_AND_PASS_DEF = "SELECT e FROM Employee AS e WHERE e.deleteFlag = 0 AND e.code =" + JPQL_PARM_CODE + "AND e.Password = :" + JPQL_PARM_PASSWORD;
+    String Q_EMP_GET_BY_CODE_AND_PASS_DEF = "SELECT e FROM Employee AS e WHERE e.deleteFlag = 0 AND e.code = :" + JPQL_PARM_CODE + " AND e.password = :" + JPQL_PARM_PASSWORD;
     // 指定した社員番号を保持する従業員の件数を取得する
     String Q_EMP_COUNT_REGISTERED_BY_CODE = ENTITY_EMP + ".countRegisteredByCode";
-    String Q_EMP_COUNT_REGISTERED_BY_CODE_DEF = "SELECT e COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE;
+    String Q_EMP_COUNT_REGISTERED_BY_CODE_DEF = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :" + JPQL_PARM_CODE;
     // 全ての日報をidの降順に取得する
     String Q_REP_GET_ALL = ENTITY_REP + ".getAll";
     String Q_REP_GET_ALL_DEF = "SELECT r FROM Report AS r ORDER BY r.id DESC";
