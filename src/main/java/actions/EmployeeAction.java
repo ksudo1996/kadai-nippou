7de390b52469eeb43ae8dc1e96e14f279217c57f
kohei.sudo <ksudo@unisrv.jp>
2023-06-22 18:59:10 +0900

    従業員destroyアクションの作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -222,4 +222,25 @@ public class EmployeeAction extends ActionBase {
             }
         }
     }
+
+    /**
+     * 論理削除を行う
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void destroy() throws ServletException, IOException {
+
+        // CSRF対策 tokenのチェック
+        if(checkToken()) {
+
+            // idを条件にzっ有業インデータを論理削除
+            service.destroy(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+            // セッションに削除官僚のフラッシュメッセージを設定
+            putSessionScope(AttributeConst.FLUSH, MessageConst.I_DELETED.getMessage());
+
+            // 一覧画面にリダイレクト
+            redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
+        }
+    }
 }
